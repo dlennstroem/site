9885abfc56d810894929d0a80799710364196a76
lennsdav <lennsdav@students.zhaw.ch>
2026-03-15 15:17:59 +0100

    start on image loading

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
+const Image = require("@11ty/eleventy-img")
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
-  eleventyConfig.addPassthroughCopy("src/assets/images")
+  eleventyConfig.addPassthroughCopy("src/assets/css/style.css")
+  // eleventyConfig.addPassthroughCopy("src/assets/images")
   eleventyConfig.addPassthroughCopy("src/scripts")
 
   eleventyConfig.addShortcode(
@@ -10,14 +11,51 @@ module.exports = function(eleventyConfig) {
         <p>${subtitle}</p>`
   )
 
+  eleventyConfig.addNunjucksAsyncShortcode("Image", async (src, alt) => {
+    if (!alt) {
+      // throw new Error(`Missing \`alt\` on image from: ${src}`)
+    }
+
+    let stats = await Image(src, {
+      widths: [25, 320, 640, 960, 1200, 1800, 2400, 4000],
+      formats: ["webp", "jpeg"],
+      urlPath: "/assets/images/",
+      outputDir: "./_site/assets/images/",
+    })
+
+    let lowsestSrc = stats["jpeg"][0]
+    const srcset = Object.keys(stats).reduce(
+      (acc, format) => ({
+        ...acc,
+        [format]: stats[format].reduce(
+          (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+          ""
+        )
+      })
+    )
+
+    const source = `<source type="image/webp" srcset="${srcset["webp"]}">`
+
+    const img = `<img
+      loading="lazy"
+      alt="${alt}"
+      src="${lowsestSrc.url}"
+      sizes="(max-width: 1024) 1024px, 100vw"
+      srcset="${srcset["jpeg"]}"
+      width="${lowsestSrc.width}"
+      height="${lowsestSrc.height}">`
+
+    return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`
+  })
+
   
 
-  const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+  const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
   eleventyConfig.addPlugin(eleventyNavigationPlugin)
 
 
   // Markdown-support:
-  
+
   // const markdownIt = require("markdown-it")
   // const markdownItAttrs = require('markdown-it-attrs')
 
@@ -29,6 +67,9 @@ module.exports = function(eleventyConfig) {
   // }).use(markdownItAttrs)
 
   // eleventyConfig.setLibrary("md", markdownLib)
+
+
+
 
 
 
